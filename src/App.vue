a10d2cff012de828abbdf2148de6d27cbb1e3992
Daiwei <daiweilu123@gmail.com>
2016-03-01 16:38:30 -0500

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="scss">
+@import './css/normalize.css';
+@import './css/skeleton.css';
+</style>
